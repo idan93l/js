5757c21d542e88725885c007136f290950dad9ee
idan.93l <idan.93lev@gmail.com>
2021-11-07 21:10:11 +0200

    Javascript

---
+++ 1.1-primitive_data_types.js
@@ -0,0 +1,14 @@
+const string = 'string';
+const num = 6;
+const objct = {};
+const boolean = true;
+const n = null;
+const sym = Symbol('s')
+
+console.log(typeof string);
+console.log(typeof num);
+console.log(typeof objct);
+console.log(typeof boolean);
+console.log(typeof n);
+console.log(typeof und);
+console.log(typeof sym);
