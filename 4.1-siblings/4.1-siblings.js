614b604f66ab3e0111156d1a4336b2de0a52e583
idan.93l <idan.93lev@gmail.com>
2021-11-08 22:25:53 +0200

    Siblings

--- 4.1-siblings/4.1-siblings.js
+++ 4.1-siblings/4.1-siblings.js
@@ -1,12 +1,13 @@
 const numSiblings = prompt(`How many siblings do you have?`);
-if (numSiblings == 1) {
-  console.log(`${numSiblings} sibling`);
+const numSiblings1 = parseInt(numSiblings)
+if (numSiblings1 === 1) {
+  console.log(`${numSiblings1} sibling`);
 }
 
-else if (numSiblings > 1) {
+else if (numSiblings1 > 1) {
   console.log(`More than 1 sibling`);
 }
 
-else if (numSiblings < 1) {
+else if (numSiblings1 < 1) {
   console.log(`No siblings`)
 }
