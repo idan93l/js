5d9d73f733160579198366c3ccf32d6a0fb2319b
idan.93l <idan.93lev@gmail.com>
2021-11-09 10:03:35 +0200

    Basketball

--- 4.3-basketball/basketball.js
+++ 4.3-basketball/basketball.js
@@ -1,30 +1,39 @@
 const john = [89, 120, 103];
 const mike = [116, 94, 123];
 const mary = [97, 134, 105]
-const avg = a => ((a[0] + a[1] + a[2])/a.length);
+const avg = a => ((a[0] + a[1] + a[2]) / a.length);
 const johnAvg = avg(john);
 const mikeAvg = avg(mike);
 const maryAvg = avg(mary);
 
-
-if (johnAvg > mikeAvg && johnAvg > maryAvg) {
-  console.log(`John is the winner with the score of ${johnAvg}!`)
+const printResults = (johnAvg, mikeAvg, maryAvg) => {
+  if (johnAvg > mikeAvg && johnAvg > maryAvg) {
+    return `John is the winner with the score of ${johnAvg}!`;
   }
 
-else if (mikeAvg > johnAvg && mikeAvg > maryAvg) {
-  console.log(`Mike is the winner with the score of ${mikeAvg}!`)
+  else if (mikeAvg > johnAvg && mikeAvg > maryAvg) {
+    return `Mike is the winner with the score of ${mikeAvg}!`
   }
 
-else {
-  console.log(`Mary is the winner with the score of ${maryAvg}!`)
+  else {
+    return `Mary is the winner with the score of ${maryAvg}!`
   }
+}
 
-if (johnAvg === mikeAvg && johnAvg === maryAvg) {
-  console.log(`It's a draw between everyone!`);
-  } else if (johnAvg === mikeAvg) {
-  console.log(`It's a draw between John and Mike`);
-  } else if (johnAvg === maryAvg) {
-  console.log(`It's a draw between John and Mary`);
-  } else if (mikeAvg === maryAvg) {
-  console.log(`It's a draw between Mike and Mary`)
+const printDraw = (johnAvg, mikeAvg, maryAvg) => {
+  if (johnAvg === mikeAvg && johnAvg === maryAvg) {
+    return `It's a draw between everyone!`;
   }
+  else if (johnAvg === mikeAvg) {
+    return `It's a draw between John and Mike`;
+  }
+  else if (johnAvg === maryAvg) {
+    return `It's a draw between John and Mary`;
+  }
+  else if (mikeAvg === maryAvg) {
+    return `It's a draw between Mike and Mary`;
+  }
+}
+
+console.log(printResults(johnAvg, mikeAvg, maryAvg));
+console.log(printDraw(johnAvg, mikeAvg, maryAvg));
