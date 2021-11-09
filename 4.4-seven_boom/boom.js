ca958b5bb5bd11d9b6764586bd6a01e0e8153acb
idan.93l <idan.93lev@gmail.com>
2021-11-09 02:10:31 +0200

    Seven Boom

---
+++ 4.4-seven_boom/boom.js
@@ -0,0 +1,19 @@
+const n = prompt(`Enter a number`);
+
+function sevenBoom(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 7 === 0 && i.toString().match(/7/))  {
+      console.log(`BOOM-BOOM`);
+    }
+
+    else if (i % 7 === 0 || i.toString().match(/7/)) {
+      console.log(`BOOM`)
+    }
+
+    else {
+      console.log(i);
+    }
+  }
+}
+
+sevenBoom(n);
