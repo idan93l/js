e5c5ef416617f17b440fdf6ca64d04d730439e92
idan.93l <idan.93lev@gmail.com>
2021-11-16 01:01:45 +0200

    Library

---
+++ 12.1-library/Library.js
@@ -0,0 +1,24 @@
+const library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false
+  }
+];
+
+const availableBook = library => {
+  const book = library.filter(book => book.readingStatus === true)
+  console.log(book);
+}
+
+availableBook(library);
