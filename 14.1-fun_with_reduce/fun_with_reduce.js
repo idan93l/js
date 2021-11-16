94a63fd1c99c2272e695b98795808ba7c76f2998
idan.93l <idan.93lev@gmail.com>
2021-11-16 15:41:45 +0200

    Fun with reduce

---
+++ 14.1-fun_with_reduce/fun_with_reduce.js
@@ -0,0 +1,22 @@
+const arr = [2, 3, 1, 2];
+
+// 1.
+const max = arr.reduce((max, currentVal) => {
+return Math.max(max, currentVal);
+});
+
+console.log(max);
+
+// 2.
+const sumOfEven = arr.reduce((sum, currentVal) => {
+  return currentVal % 2 === 0 ? sum + currentVal : sum;
+}, 0);
+
+console.log(sumOfEven);
+
+// 3.
+const avg = arr.reduce((sum, currentVal) => {
+return sum + currentVal;
+}) / arr.length;
+
+console.log(avg);
