505512a6623c388cc57c920ccf0f812210052c67
idan.93l <idan.93lev@gmail.com>
2021-11-17 15:39:33 +0200

    Sum of even

---
+++ 15.3-sum_of_even/sum_of_even.js
@@ -0,0 +1,9 @@
+function getSumOfEven(arr) {
+  let sum = arr[2] + arr[4] + arr[6] + arr[8] + arr[10];
+  return sum;
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// 1. line 2
+// 2. node debug terminal
+// 3. wrong array slots: 2, 4, 6, 8, 10 means - 3th, 5th, 7th, 9th, 11th;
