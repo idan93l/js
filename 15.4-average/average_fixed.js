007b691287f3891dac11e56803ddd055062e592c
idan.93l <idan.93lev@gmail.com>
2021-11-17 18:05:17 +0200

    Average

---
+++ 15.4-average/average_fixed.js
@@ -0,0 +1,13 @@
+function calcAverage(arr) {
+  var sum;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+calcAverage([85, 90, 92]);
+
+  // 1. lines 2, 6
+  // 2. node debugger
+  // 3. line 2- sum has to be assigned with a number
+// line 6 - sum should be divided by arr.length
